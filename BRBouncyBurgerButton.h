d41ab270c0313b7d139689013f907981b8edd8ad
Aaron Voisine <voisine@gmail.com>
2014-06-10 19:16:53 -0700

    added email to copyright

--- BRBouncyBurgerButton.h
+++ BRBouncyBurgerButton.h
@@ -2,7 +2,7 @@
 //  BRBouncyBurgerButton.h
 //
 //  Created by Aaron Voisine on 6/5/14.
-//  Copyright (c) 2014 Aaron Voisine.
+//  Copyright (c) 2014 Aaron Voisine <voisine@gmail.com>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
